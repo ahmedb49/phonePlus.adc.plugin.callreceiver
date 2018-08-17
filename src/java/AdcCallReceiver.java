9dd3812be6e56dd03f76a2d877e48ab2437625eb
Ahmed Chtara <ahmed.chtara@gmail.com>
2018-08-17 15:55:55 +0100

    first commit

---
+++ src/java/AdcCallReceiver.java
@@ -0,0 +1,94 @@
+package io.adc.adccallreceiver;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.telephony.TelephonyManager;
+
+
+import org.json.JSONException;
+import org.json.JSONArray;
+
+import io.adc.adccallreceiver.Test;
+
+public class AdcCallReceiver extends CordovaPlugin {
+
+
+	
+    public static  CallbackContext cbc;
+  
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        
+        AdcCallReceiver.cbc=callbackContext;
+        return true;
+    }
+       
+
+    public static class CallReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+           
+            String msg="";
+			if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                msg="RINGING"; 
+               // sendResult(msg);
+			   Intent local = new Intent();
+				local.setAction("com.hello.action");
+				context.sendBroadcast(local);
+            }
+            else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+                msg="OFFHOOK";
+               // sendResult(msg);
+			   Intent local = new Intent();
+				local.setAction("com.hello.action");
+				context.sendBroadcast(local);
+                 
+            }
+            else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                msg="IDLE";
+				//sendResult(msg);
+				final Handler handler = new Handler();
+				handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent trIntent = new Intent("android.intent.action.MAIN");
+                    trIntent.setClass(context, Test.class);
+                    trIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                    trIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                    Intent in = new Intent(context, Test.class);
+                    context.startActivity(trIntent);
+
+
+                }
+            }, 2000);
+            }
+            
+            
+        }
+
+		public void sendResult(String msg){
+			PluginResult result = new PluginResult(PluginResult.Status.OK, msg);
+			result.setKeepCallback(true);
+
+            if (AdcCallReceiver.cbc!=null){
+			AdcCallReceiver.cbc.sendPluginResult(result);
+            }
+            
+        } 
+            
+            
+	}
+
+}   
+   
+
+
+
+
---
+++ src/java/Test.java
@@ -0,0 +1,104 @@
+package io.adc.adccallreceiver;
+import android.app.Activity;
+import android.app.Application;
+import android.content.BroadcastReceiver;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Test extends Activity {
+     Context ctx ;
+     LinearLayout ly;
+    WindowManager wm;
+    TextView textView;
+    Button yes;
+    Button no;
+
+    int lastAction;
+
+    RelativeLayout rel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
+                       // | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        this.setFinishOnTouchOutside(false);
+        super.onCreate(savedInstanceState);
+
+		Application app = cordova.getActivity().getApplication();
+		String package_name = app.getPackageName();
+        Resources resources = app.getResources();
+        setContentView(resources.getIdentifier("activity_test", "layout", package_name));
+		
+        //setContentView(R.layout.activity_test);
+
+        yes=findViewById(resources.getIdentifier("yes", "id", package_name));
+        no=findViewById(resources.getIdentifier("no", "id", package_name));
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PackageManager pm = getPackageManager();
+                Intent launchIntent = pm.getLaunchIntentForPackage("io.ionic.starter");
+                finish();
+                startActivity(launchIntent);
+            }
+        });
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+       
+      
+
+        ViewGroup view = (ViewGroup)getWindow().getDecorView();
+        view.setBackgroundColor(0xFF00FF00);
+     
+
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction("com.hello.action");
+        registerReceiver(receiver, filter);
+    }
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+           
+            finishAndRemoveTask();
+        }
+
+
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //wm.removeViewImmediate(ly);
+
+        unregisterReceiver(receiver);
+    }
+}
